135877e9af6618ac8bc886491bded8be5e1d2f53
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-03-06 10:18:33 +0700

    Changed ExportToExcel to ExportToCsv

--- ShipmentDataGrids.Lib/ShipmentDataGridControl.cs
+++ ShipmentDataGrids.Lib/ShipmentDataGridControl.cs
@@ -315,6 +315,73 @@ namespace ShipmentDataGrids.Lib
         }
 
 
+        /// <summary>
+        /// Экспорт таблицы в CSV
+        /// </summary>
+        private void ExportToCsv()
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog
+                {
+                    Filter = "CSV (*.csv)|*.csv",
+                    FilterIndex = 2,
+                    FileName = $"Shipment_{DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss")}"
+                };
+                
+                bool fileError = false;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(sfd.FileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("Невозможно записать данные на этот диск" + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            int columnCount = dataGridView1.Columns.Count;
+                            string columnNames = "";
+                            string[] outputCsv = new string[dataGridView1.Rows.Count + 1];
+                            for (int i = 0; i < columnCount; i++)
+                            {
+                                columnNames += dataGridView1.Columns[i].HeaderText.ToString() + ";";
+                            }
+                            outputCsv[0] += columnNames;
+
+                            for (int i = 1; (i - 1) < dataGridView1.Rows.Count; i++)
+                            {
+                                for (int j = 0; j < columnCount; j++)
+                                {
+                                    outputCsv[i] += dataGridView1.Rows[i - 1].Cells[j].Value.ToString() + ";";
+                                }
+                            }
+
+                            File.WriteAllLines(sfd.FileName, outputCsv, Encoding.UTF8);
+                            MessageBox.Show("Экспорт завершен", "Info");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Нечего экспортировать", "Info");
+            }
+        }
+
+
         #endregion
 
 
@@ -464,7 +531,8 @@ namespace ShipmentDataGrids.Lib
 
         private void buttonExportExcel_Click(object sender, EventArgs e)
         {
-            ExportToExcel();
+            //ExportToExcel();
+            ExportToCsv();
         }
 
 
