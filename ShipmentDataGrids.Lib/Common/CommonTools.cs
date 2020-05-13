73e575d14cdcddbfff97612f4a2e777bc0529e60
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-05-14 00:37:42 +0700

    В CommonTools добавлен метод GetConfig считывающий из JSON конфигурацию подключения к БД

--- ShipmentDataGrids.Lib/Common/CommonTools.cs
+++ ShipmentDataGrids.Lib/Common/CommonTools.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
+using Newtonsoft.Json;
 using ShipmentDataGrids.Lib.Interfaces;
 
 namespace ShipmentDataGrids.Lib.Common
 {
     static class CommonTools
     {
+        /// <summary>
+        /// Возвращает строку подключения
+        /// </summary>
+        /// <param name="config"> Конфигурация </param>
+        /// <returns></returns>
         public static string GetConnectionString(IConfig config)
         {
 
-            var conString;
+            string conString = null;
 
             switch (config.DbType)
             {
@@ -32,14 +40,42 @@ namespace ShipmentDataGrids.Lib.Common
 
                 case "MySql":
 
-                    conString = ""
-                    
+                    conString = $"Server = localhost; Database = {config.DbName}; Uid = {config.UserName}; Pwd = {config.Password};";
                     break;
-            
 
+                case "PostgreSql":
+                    // TODO: VAP; Вставить строку
+                    conString = null;
+                    break;
+
+                case "Sqlite":
+                    // TODO: VAP; Вставить строку
+                    conString = null;
+                    break;
             default:
                     break;
             }
+
+            return conString;
         }
+
+        /// <summary>
+        /// Считывает конфигурацию из файла
+        /// </summary>
+        /// <param name="filename"> JSON файл с конфигурацией </param>
+        /// <returns></returns>
+        public static IConfig GetConfig(string filename)
+        {
+            IConfig cfg = null;
+
+                using (StreamReader file = File.OpenText(filename))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    cfg = (IConfig)serializer.Deserialize(file, typeof(IConfig));
+                }
+
+            return cfg;
+        }
+        
     }
 }
