2989f6a22a377204357ae1b2d32d48924557bd0b
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-05-14 01:18:14 +0700

    Добавлен класс DbService из которого будет производиться работа с БД

---
+++ ShipmentDataGrids.Lib/Services/DbService.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using ShipmentDataGrids.Lib.Common;
+using ShipmentDataGrids.Lib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentDataGrids.Lib.Services
+{
+    class DbService
+    {
+        #region Fields
+        private readonly IDbConnection _dbConnection;
+        private readonly string _sqlQuery;
+
+        #endregion
+
+        #region Constructors
+        public DbService(IConfig cfg)
+        {
+            // TODO: VAP; Создать dbConnection для каждой базы данных
+
+            _sqlQuery = CommonTools.SqlQueryAll;
+        }
+
+        #endregion
+
+
+        #region Methods
+        public IEnumerable<IShipment> GetShipments() => _dbConnection.Query<IShipment>(_sqlQuery)
+                                                        .ToList();
+        #endregion
+
+
+    }
+}
