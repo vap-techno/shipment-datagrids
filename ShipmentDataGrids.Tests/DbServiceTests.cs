c7109b46b990d1f5a35e8a7e5f566e434840a5cb
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-05-14 17:03:12 +0700

    Добавлен метод генерации тестовых данных в базу Sqlite

--- ShipmentDataGrids.Tests/DbServiceTests.cs
+++ ShipmentDataGrids.Tests/DbServiceTests.cs
@@ -19,5 +19,33 @@ namespace ShipmentDataGrids.Tests
 
         }
 
+        private void InsertEntity(IShipment entity)
+        {
+
+        }
+
+        private List<IShipment> GenerateTestEntities(int qnt = 100)
+        {
+            List<IShipment> lst = new List<IShipment>();
+            
+            for (int i = 0; i < qnt; i++)
+            {
+                IShipment s = new Shipment();
+
+                s.Id = i;
+                s.Ts = DateTime.Now.AddHours(-i);
+                s.PostName = "Пост 1";
+                s.TimeBegin = s.Ts.AddMinutes(-10);
+                s.TimeEnd = s.Ts;
+                s.SetPoint = 1000 + 0.1 * i;
+                s.ResultWeight = s.SetPoint + (new Random()).NextDouble();
+                s.ResultVolume = s.SetPoint + (new Random()).NextDouble();
+
+
+            }
+
+            return lst;
+        }
+
     }
 }
