374879557462551a48150fee1807e71abae15327
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-05-14 10:45:04 +0700

    Отредактирован IShipment, в него добавлены свойства

--- ShipmentDataGrids.Lib/Interfaces/IShipment.cs
+++ ShipmentDataGrids.Lib/Interfaces/IShipment.cs
@@ -8,5 +8,75 @@ namespace ShipmentDataGrids.Lib.Interfaces
 {
     interface IShipment
     {
+        /// <summary>
+        /// Идентификатор записи
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        /// Номер поста налива
+        /// </summary>
+        string PostName { get; set; }
+
+        /// <summary>
+        /// Метка времени
+        /// </summary>
+        DateTime Ts { get; set; }
+
+        /// <summary>
+        /// Дата начала налива
+        /// </summary>
+        DateTime TimeBegin { get; set; }
+
+        /// <summary>
+        /// Дата окончания налива
+        /// </summary>
+        DateTime TimeEnd { get; set; }
+
+        /// <summary>
+        /// Уставка
+        /// </summary>
+        double SetPoint { get; set; }
+
+        /// <summary>
+        /// Отгруженное значение по массе
+        /// </summary>
+        double ResultWeight { get; set; }
+
+        /// <summary>
+        /// Отгруженное значение по объему
+        /// </summary>
+        double ResultVolume { get; set; }
+
+        /// <summary>
+        /// Ед. измерения (тип измерения)
+        /// </summary>
+        int Unit { get; set; }
+
+        /// <summary>
+        /// Плотность продукта
+        /// </summary>
+        double Density { get; set; }
+
+        /// <summary>
+        /// Температура продутка
+        /// </summary>
+        double Temperature { get; set; }
+
+        /// <summary>
+        /// Наименование продукта
+        /// </summary>
+        string ProductName { get; set; }
+
+        /// <summary>
+        /// Наименование цистерны
+        /// </summary>
+        string TankName { get; set; }
+
+        /// <summary>
+        /// Статус налива
+        /// </summary>
+        int FinalStatus { get; set; }
+
     }
 }
