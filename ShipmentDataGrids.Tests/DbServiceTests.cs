33dc5c59eeb226d5472c1b79fbed89861d42869d
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-05-14 16:32:19 +0700

    Добавлен проект для тестов

---
+++ ShipmentDataGrids.Tests/DbServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ShipmentDataGrids.Lib.Models;
+using ShipmentDataGrids.Lib.Interfaces;
+
+
+namespace ShipmentDataGrids.Tests
+{
+    [TestFixture]
+    public class DbServiceTests
+    {
+        [Test]
+        public void Sqlite_reading()
+        {
+
+        }
+
+    }
+}
