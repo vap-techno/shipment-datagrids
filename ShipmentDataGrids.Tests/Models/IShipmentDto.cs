4fdf1980a8743200560e6159c16c88aaee0b2ba0
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-05-15 20:50:47 +0700

    В тестах добавлен объект, который добавлен класс ShipmentDto для записи в БД

---
+++ ShipmentDataGrids.Tests/Models/IShipmentDto.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentDataGrids.Tests.Models
+{
+    interface IShipmentDto
+    {
+
+        /// <summary>
+        /// Номер поста налива
+        /// </summary>
+        string PostName { get; set; }
+
+        /// <summary>
+        /// Дата начала налива
+        /// </summary>
+        DateTime TimeBegin { get; set; }
+
+        /// <summary>
+        /// Дата окончания налива
+        /// </summary>
+        DateTime TimeEnd { get; set; }
+
+        /// <summary>
+        /// Уставка
+        /// </summary>
+        double SetPoint { get; set; }
+
+        /// <summary>
+        /// Отгруженное значение по массе
+        /// </summary>
+        double ResultWeight { get; set; }
+
+        /// <summary>
+        /// Отгруженное значение по объему
+        /// </summary>
+        double ResultVolume { get; set; }
+
+        /// <summary>
+        /// Ед. измерения (тип измерения)
+        /// </summary>
+        int Unit { get; set; }
+
+        /// <summary>
+        /// Плотность продукта
+        /// </summary>
+        double Density { get; set; }
+
+        /// <summary>
+        /// Температура продутка
+        /// </summary>
+        double Temperature { get; set; }
+
+        /// <summary>
+        /// Наименование продукта
+        /// </summary>
+        string ProductName { get; set; }
+
+        /// <summary>
+        /// Наименование цистерны
+        /// </summary>
+        string TankName { get; set; }
+
+        /// <summary>
+        /// Статус налива
+        /// </summary>
+        int FinalStatus { get; set; }
+    }
+}
---
+++ ShipmentDataGrids.Tests/Models/ShipmentDto.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShipmentDataGrids.Lib.Interfaces;
+
+namespace ShipmentDataGrids.Tests.Models
+{
+    class ShipmentDto : IShipmentDto
+    {
+
+        public ShipmentDto()
+        {
+
+        }
+
+        public ShipmentDto(IShipment shipment)
+        {
+            Map(shipment);
+        }
+
+        private void Map(IShipment shipment)
+        {
+            PostName = shipment.PostName;
+            TimeBegin = shipment.TimeBegin;
+            SetPoint = shipment.SetPoint;
+            ResultWeight = shipment.ResultWeight;
+            ResultVolume = shipment.ResultVolume;
+
+            switch (shipment.Unit)
+            {
+                case "кг":
+                    Unit = 0;
+                    break;
+
+                case "л":
+                    Unit = 1;
+                    break;
+
+                case "см":
+                    Unit = 2;
+                    break;
+
+                default:
+                    Unit = 0;
+                    break;
+
+            }
+
+            Density = shipment.Density;
+            Temperature = shipment.Temperature;
+            ProductName = shipment.ProductName;
+            TankName = shipment.TankName;
+
+            switch(shipment.FinalStatus)
+            {
+                case "Завершена":
+                    FinalStatus = 1;
+                    break;
+
+                case "Остановлена":
+                    FinalStatus = 2;
+                    break;
+
+                default:
+                    FinalStatus = 0;
+                    break;
+            }
+
+
+        }
+
+        public string PostName { get; set; }
+        public DateTime TimeBegin { get; set; }
+        public DateTime TimeEnd { get; set; }
+        public double SetPoint { get; set; }
+        public double ResultWeight { get; set; }
+        public double ResultVolume { get; set; }
+        public int Unit { get; set; }
+        public double Density { get; set; }
+        public double Temperature { get; set; }
+        public string ProductName { get; set; }
+        public string TankName { get; set; }
+        public int FinalStatus { get; set; }
+    }
+}
