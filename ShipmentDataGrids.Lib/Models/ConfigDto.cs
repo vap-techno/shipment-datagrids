519b9efcb0fa10e14f342a65363fd254f857ae2b
Anatoly Veshtemov <veshtemov@hotmail.com>
2020-05-31 00:09:01 +0500

    Добавлен промежуточный класс ConfigDto

---
+++ ShipmentDataGrids.Lib/Models/ConfigDto.cs
@@ -0,0 +1,16 @@
+﻿using ShipmentDataGrids.Lib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ShipmentDataGrids.Lib.Models
+{
+    class ConfigDto
+    {
+        [JsonProperty(PropertyName = "Shipment", Required = Required.Default)]
+        public IConfig Config { get; set; }
+    }
+}
